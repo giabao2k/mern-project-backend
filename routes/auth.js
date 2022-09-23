1302e6e5f7f993912e1f520e9d701e24e5826512
giabao2k <ngonguyengiabao2000@gmail.com>
2022-09-23 15:20:48 +0700

    done api register login

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,7 @@ router.post('/register', async (req, res) => {
     // console.log(req.body);
     // Simple validation (xác nhận đơn giản)
     if (!username || !password) {
-        res.status(400).json({ success: false, message: 'không có username and/or password' });
+        return res.status(400).json({ success: false, message: 'không có username and/or password' });
     }
     try {
         // Check for existing user( xem người dùng có tồn tại hay không)
@@ -31,6 +31,7 @@ router.post('/register', async (req, res) => {
         //all good
         const hashedPassword = await bcrypt.hash(password, 12);
         const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
         // Return token
         const accessToken = jwt.sign(
             {
@@ -39,11 +40,53 @@ router.post('/register', async (req, res) => {
             process.env.SECRET_KEY,
             { expiresIn: 36000 },
         );
-        console.log(accessToken);
-        await newUser.save();
-        res.json({ success: true, message: 'user created', accessToken });
-        console.log(newUser);
-    } catch (error) {}
+        // console.log(accessToken);
+        res.json({ success: true, message: 'đăng kí thành công', accessToken });
+        // console.log(newUser);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+// @route POST api/auth/login
+// @description Register user (mô tả)
+// access Public (trạng thái)
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    console.log(req.body);
+    // Simple validation (xác nhận đơn giản)
+    if (!username || !password) {
+        return res.status(400).json({ success: false, message: 'không có username and/or password' });
+    }
+    try {
+        // Check for existing user( xem người dùng có tồn tại hay không)
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ success: false, message: 'tài khoản không đúng' });
+        }
+        // Username found (tên đăng nhập đúng=> check pass)
+        const passwordValid = await bcrypt.compare(password, user.password);
+
+        if (!passwordValid) {
+            return res.status(400).json({ success: false, message: 'mật khẩu không đúng' });
+        } else {
+            // All good
+            // Return token
+            const accessToken = jwt.sign(
+                {
+                    idUser: user._id,
+                },
+                process.env.SECRET_KEY,
+                { expiresIn: 36000 },
+            );
+            res.json({ success: true, message: 'người dùng đăng nhập thành công', accessToken });
+            console.log(newUser);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 });
 
 module.exports = router;
